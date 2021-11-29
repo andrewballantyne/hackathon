afdceaffa174fd8c513f7b3c35f82120e105ab3f
Christian Vogt <cvogt@redhat.com>
2021-11-29 11:45:43 -0500

    add typescript parser to styleguidist

--- src/components/HelloWorld/HelloWorld.tsx
+++ src/components/HelloWorld/HelloWorld.tsx
@@ -1,3 +1,10 @@
-const HelloWorld = () => 'Hello World!';
+type HelloWorldProps = {
+  /** the number to include in the title */
+  count: number;
+};
+
+const HelloWorld: React.FC<HelloWorldProps> = ({ count }) => (
+  <span>{`Hello World! (${count})`}</span>
+);
 
 export default HelloWorld;
