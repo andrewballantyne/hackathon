76daf6d617bad43c36659ac320f73253efd4028f
Christian Vogt <cvogt@redhat.com>
2021-11-30 16:57:26 -0500

    remove DashboardCard since the frame owns the card

--- src/components/card-structure/index.ts
+++ src/components/card-structure/index.ts
@@ -1,4 +1,3 @@
 export { default as DashboardCardFrame } from './DashboardCardFrame';
-export { default as DashboardCard } from './DashboardCard';
 export { default as DashboardCardTitle } from './DashboardCardTitle';
 export { default as DashboardCardBody } from './DashboardCardBody';
