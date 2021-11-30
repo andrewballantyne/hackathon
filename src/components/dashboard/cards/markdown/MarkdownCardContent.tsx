d2063c19d42a1c95ee0678545c4b12ccd7c702bc
Andrew Ballantyne <aballant@redhat.com>
2021-11-30 17:07:58 -0500

    Fill out Card types

--- src/components/dashboard/cards/markdown/MarkdownCardContent.tsx
+++ src/components/dashboard/cards/markdown/MarkdownCardContent.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { DashboardCardTitle, DashboardCardBody } from '../../../card-structure';
+import {
+  DashboardCardContent,
+  DashboardCardTitle,
+  DashboardCardBody,
+} from '../../../card-structure';
 import { MarkdownCardData } from './types';
 
 const MarkdownCardContent: React.FC<MarkdownCardData> = ({ title, description, markdown }) => (
-  <>
+  <DashboardCardContent>
     <DashboardCardTitle title={description}>{title}</DashboardCardTitle>
     <DashboardCardBody>{markdown}</DashboardCardBody>
-  </>
+  </DashboardCardContent>
 );
 
 export default MarkdownCardContent;
