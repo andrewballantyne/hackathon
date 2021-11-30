52eb299c2b39f7213a36df7e7eb8523c2fcc33ed
Andrew Ballantyne <aballant@redhat.com>
2021-11-30 12:53:05 -0500

    Remove Styled Components

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { App } from './App';
 import { DashboardApp as App } from './components/Dashboard/DashboardApp';
 import reportWebVitals from './reportWebVitals';
-import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyles = createGlobalStyle`
-  body, html, #root {
-    height: 100%;
-  }
-
-  body {
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-
-  code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-    monospace;
-  }
-`;
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
