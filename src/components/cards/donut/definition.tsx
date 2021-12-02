4d5d4b1259d3f18a715a22e0227d2f753d76cdad
Christian Vogt <cvogt@redhat.com>
2021-12-02 17:59:45 -0500

    add preview image to donut chart

--- src/components/cards/donut/definition.tsx
+++ src/components/cards/donut/definition.tsx
@@ -2,13 +2,14 @@ import DonutChartIcon from '@patternfly/react-icons/dist/esm/icons/chart-pie-ico
 import { DonutCardData } from './types';
 import { CardDefinition } from '../../../types';
 import { CardType } from '../types';
+import { donutPreview } from '../../../test/base64-images';
 
 const definition: CardDefinition<DonutCardData> = {
   id: CardType.DONUT,
   label: 'Donut Chart',
   description: 'Displays a donut chart.',
   icon: <DonutChartIcon />,
-  images: [],
+  images: [donutPreview],
   settingsComponent: () =>
     import('./DonutCardSettings' /* webpackChunkName: "pf-dashboard-card-donut" */),
   contentComponent: () =>
